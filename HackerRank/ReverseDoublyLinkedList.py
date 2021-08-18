f7ad1fb3a5fc665d9e3ed947f449e8b332cbcb14
Samantha <samantha_ann_west@hotmail.ca>
2021-08-17 22:18:35 -0400

    Doubly Linked List

---
+++ HackerRank/ReverseDoublyLinkedList.py
@@ -0,0 +1,76 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def reverse(llist):
+    prev = None
+    head = llist
+    
+    # Loop while head node
+    while head:     
+        # Create buffer for next node for while loop
+        next_head = head.next
+        head.next = prev # Next is now previous
+        prev = head # Previous is now next
+        head = next_head # Set next head for while loop
+    
+    # Return last node in list
+    return prev
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
